cdd3bafb80016b546bcd326706c96cdb11ac28f9
Evstropova Maria <Evstropova@rta.ru>
2016-04-13 18:29:42 +0300

    JUnit testing first commit

---
+++ myapplication/src/androidTest/java/com/evstropova/myapplication/MainActivityTest.java
@@ -0,0 +1,35 @@
+package com.evstropova.myapplication;
+
+import org.junit.Test;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.AfterClass;
+
+public class MainActivityTest {
+    private MainActivity mainActivity;
+
+    @Test
+    public void testReadMessage() {
+
+    }
+
+    @Test
+    public void testReadStringsArray() {
+
+    }
+
+    @Test
+    public void testReadCover() {
+
+    }
+
+    @Test
+    public void testGetStatistics() {
+
+    }
+
+    @Test
+    public void testModifyDescription() {
+
+    }
+}
